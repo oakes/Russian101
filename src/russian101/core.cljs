4ebc6f780735a1b00e5e0f8945db4466c94b07bf
oakes <zsoakes@gmail.com>
2017-07-19 17:15:43 -0400

    Save current lesson in state when tapping on list

--- src/russian101/core.cljs
+++ src/russian101/core.cljs
@@ -15,36 +15,47 @@
 (def image (partial create-element (.-Image ReactNative)))
 (def touchable-highlight (partial create-element (.-TouchableHighlight ReactNative)))
 (def listview (partial create-element (.-ListView ReactNative)))
+(def touchable-opacity (partial create-element (.-TouchableOpacity ReactNative)))
 
 (def logo-img (js/require "./images/cljs.png"))
 
 (defn alert [title]
   (.alert (.-Alert ReactNative) title))
 
-(defonce app-state (atom {:greeting "Hello Clojure in iOS and Android!"}))
+(defonce app-state (atom {:current-lesson nil
+                          :current-page nil}))
 
-(def menu-rows [["Alphabet" "алфавит"]
-                ["Meeting People" "Знакомство"]
-                ["Family" "семья"]
-                ["Where do you work?" "Где вы работаете?"]
-                ["Where do you live?" "Где вы живете?"]
-                ["Shopping" "покупки"]
-                ["In the restaurant" "В ресторане"]
-                ["Transportation" "транспорт"]
-                ["In the hotel" "В гостинице"]
-                ["The telephone" "телефон"]])
+(def menu-rows [; Title Subtitle Lession
+                ["Alphabet" "алфавит" 1]
+                ["Meeting People" "Знакомство" 2]
+                ["Family" "семья" 3]
+                ["Where do you work?" "Где вы работаете?" 4]
+                ["Where do you live?" "Где вы живете?" 5]
+                ["Shopping" "покупки" 6]
+                ["In the restaurant" "В ресторане" 7]
+                ["Transportation" "транспорт" 8]
+                ["In the hotel" "В гостинице" 9]
+                ["The telephone" "телефон" 10]])
+
+(def page-count [35 9 8 13 8 27 23 18 18 24])
 
 (def ds (-> (ReactNative.ListView.DataSource.
-              (clj->js {:rowHasChanged (fn [r1 r2] (not= r1 r2))}))
+              (clj->js {:rowHasChanged not=}))
             (.cloneWithRows (clj->js menu-rows))))
 
 (defc AppRoot < rum/reactive [state]
-  (listview {:style {:margin 20}
-             :dataSource ds
-             :renderRow (fn [row-data]
-                          (view {:style {:margin 5}}
-                            (text {:style {:fontSize 30}} (aget row-data 0))
-                            (text {:style {:fontSize 20}} (aget row-data 1))))}))
+  (let [{:keys [current-lesson current-page]} (rum/react state)]
+    (listview {:style {:margin 20}
+               :dataSource ds
+               :renderRow (fn [row-data]
+                            (let [title (aget row-data 0)
+                                  subtitle (aget row-data 1)
+                                  lesson (aget row-data 2)]
+                              (touchable-opacity {:onPress (fn []
+                                                             (swap! state assoc :current-lesson lesson))}
+                                (view {:style {:margin 5}}
+                                  (text {:style {:fontSize 30}} title)
+                                  (text {:style {:fontSize 20}} subtitle)))))})))
 
 (defonce root-component-factory (support/make-root-component-factory))
 
