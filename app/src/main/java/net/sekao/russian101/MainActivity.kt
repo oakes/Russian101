1cf853e301b3b5f15663c88cf6d91d9df9fc27a4
oakes <zach@sekao.net>
2018-11-30 12:11:33 -0500

    Start empty kotlin project

---
+++ app/src/main/java/net/sekao/russian101/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.sekao.russian101
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
